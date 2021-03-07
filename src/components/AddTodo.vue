e922a9c763fbec21df800290c15840a1859828fc
RIKU <rank228130@gmail.com>
2021-03-07 18:58:38 +0900

    タスク名が入力されていないときにタスクを登録させないためのバリデーション機能を追加

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="addForm">
+    <p>Todoを追加する</p>
+    <!-- value は自己のコンポーネントデータを制御するから別にv-modelで良い -->
     <input class="addForm__form" type="text" v-model="value">
     <!-- 親にイベントを投げる -->
     <button class="addForm__button" @click="onSubmit">追加</button>
@@ -17,16 +19,16 @@ export default {
   },
   methods: {
     onSubmit: function () {
-      // イベントを自作できる
-      // イベントの値
-
+      if (this.value === '') {
+        alert('項目名を入力してください')
+        return
+      }
       this.$emit('addTodo', this.id, this.value, this.state)
       this.id++
       this.value = ''
       this.state = false
     }
   }
-
 }
 </script>
 
@@ -44,4 +46,10 @@ export default {
   margin-left: 16px;
 }
 
+.addForm__button{
+  border: 0;
+  background-color: #00df38;
+  color:#fff;
+  padding: 0.3em 1.5em;
+}
 </style>
